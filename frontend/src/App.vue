c57f710ad5fad317cbf76b9e2e3940bf977dad7f
Aly Abdelrazek <alyrazik@gmail.com>
2024-12-12 23:04:30 +0200

    initial commit with frontend and backend

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>Hello Vue.js!</h1>
+    <button @click="fetchData">Fetch Data</button>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "",
+    };
+  },
+  methods: {
+    async fetchData() {
+      const response = await fetch("http://127.0.0.1:8000/");
+      const data = await response.json();
+      this.message = data.message;
+    },
+  },
+};
+</script>
